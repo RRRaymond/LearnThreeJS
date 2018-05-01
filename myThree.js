8ed33065b3d75ce1f8366d011523b0024a8109b5
raymondchen <1552702@tongji.edu.cn>
2018-05-02 04:17:36 +0800

    add cubes

--- myThree.js
+++ myThree.js
@@ -8,28 +8,33 @@ window.onload = function () {
 
 var renderer;
 function initThree() {
-    width = document.getElementById('canvas-frame').clientWidth;
-    height = document.getElementById('canvas-frame').clientHeight;
+    //width = document.getElementById('canvas-frame').clientWidth;
+    //height = document.getElementById('canvas-frame').clientHeight;
+    width = window.innerWidth;
+    height = window.innerHeight;
     renderer = new THREE.WebGLRenderer({
         antialias : true
     });
     renderer.setSize(width, height);
-    document.getElementById('canvas-frame').appendChild(renderer.domElement);
+    //document.getElementById('canvas-frame').appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
     renderer.setClearColor(0xFFFFFF, 1.0);
 }
 
 var camera;
 function initCamera() {
     camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-    camera.position.x = 10;
-    camera.position.y = 10;
-    camera.position.z = 10;
+    camera.position.x = 1000;
+    camera.position.y = 1000;
+    camera.position.z = 1000;
     camera.lookAt(new THREE.Vector3(0,0,0));
 }
 
 var scene;
 function initScene() {
     scene = new THREE.Scene();
+    var axes = new THREE.AxesHelper(1000);
+    scene.add(axes);
 }
 
 var light;
@@ -58,14 +63,17 @@ function initLight() {
     scene.add(directionalLight);
 }
 
+var geometry;
+var material;
 var cube;
+var cube2;
 function initObject() {
 
-    var geometry2 = new THREE.CubeGeometry(0.95,0.95,0.95);
-    var material2 = new THREE.MeshLambertMaterial({color: 0x00ff00, wireframe: false});
-    cube = new THREE.Mesh(geometry2, material2);
+    geometry = new THREE.CubeGeometry(0.95,0.95,0.95);
+    material = new THREE.MeshLambertMaterial({color: 0x00ff00, wireframe: false});
+    cube = new THREE.Mesh(geometry, material);
     cube.position.set(0,1,0);
-    var cube2 = new THREE.Mesh(geometry2,material2);
+    cube2 = new THREE.Mesh(geometry,material);
     cube2.position.set(0,0,0);
     scene.add(cube);
     scene.add(cube2);
@@ -76,11 +84,42 @@ function render()
     delta = clock.getDelta();
     orbitControls.update(delta);
     requestAnimationFrame(render);
-    // cube.rotation.x += 0.1;
-    // cube.rotation.y += 0.1;
-    //camera.position.x+=0.1;
+    drawLine();
     renderer.render(scene, camera);
 }
+
+function drawLine() {
+    var children = scene.children;
+    for(var i=3;i<children.length;i++){
+        scene.remove(children[i]);
+    }
+
+    var directionVector = new THREE.Vector3(
+        controls.end_x-controls.start_x,
+        controls.end_y-controls.start_y,
+        controls.end_z-controls.start_z
+    );
+    var mainDirection = Math.max(Math.abs(directionVector.x), Math.abs(directionVector.y), Math.abs(directionVector.z));
+    var x = controls.start_x;
+    var y = controls.start_y;
+    var z = controls.start_z;
+    var dx = directionVector.x/mainDirection;
+    var dy = directionVector.y/mainDirection;
+    var dz = directionVector.z/mainDirection;
+    for(var j=0;j<mainDirection;j++){
+        var cube = new THREE.Mesh(geometry, material);
+        cube.position.set(Math.round(x), Math.round(y), Math.round(z));
+        if(j === Math.round(mainDirection/2)){
+            orbitControls.target = new THREE.Vector3(x, y, z);
+        }
+        x += dx;
+        y += dy;
+        z += dz;
+        scene.add(cube);
+    }
+
+}
+
 function threeStart() {
     initGUI();
     initThree();
@@ -88,9 +127,10 @@ function threeStart() {
     initScene();
     initLight();
     initObject();
+
+
+
     initOribitControls();
-
-
     render();
 }
 
@@ -101,10 +141,13 @@ function initOribitControls() {
     //新建一个轨道控制器
     orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
     orbitControls.target = new THREE.Vector3(0, 0, 0);//控制焦点
-    orbitControls.autoRotate = true;//将自动旋转关闭
-    orbitControls.autoRotateSpeed = controls.rotationSpeed;
+    orbitControls.autoRotate = true;//将自动旋转打开
+    orbitControls.autoRotateSpeed = 5;
     clock = new THREE.Clock();//用于更新轨道控制器
 }
+
+
+
 
 var controls;
 function  initGUI() {
@@ -112,24 +155,29 @@ function  initGUI() {
     controls = new function () {
         this.rotationSpeed = 5;
         this.autoRotate = true;
-        this.start_x = 0;
-        this.start_y = 0;
-        this.start_z = 0;
-        this.end_x = 10;
-        this.end_y = 15;
-        this.end_z =20;
+        this.start_x = 211;
+        this.start_y = 106;
+        this.start_z = 326;
+        this.end_x = 433;
+        this.end_y = 985;
+        this.end_z =666;
     };
 
     //创建dat.GUI，传递并设置属性
     var gui = new dat.GUI();
+    //gui.domElement.style = 'position:absolute;top:0px;left:0px;width:180px';
     var autoRotateController =  gui.add(controls, 'autoRotate');
     var rotationSpeedController = gui.add(controls, 'rotationSpeed', 0, 20);
-    gui.add(controls, 'start_x');
-    gui.add(controls, 'start_y');
-    gui.add(controls, 'start_z');
-    gui.add(controls, 'end_x');
-    gui.add(controls, 'end_y');
-    gui.add(controls, 'end_z');
+    var f_start = gui.addFolder("Start Point");
+    f_start.add(controls, 'start_x', 0, 1000);
+    f_start.add(controls, 'start_y', 0, 1000);
+    f_start.add(controls, 'start_z', 0, 1000);
+    f_start.open();
+    var f_end = gui.addFolder("End Point");
+    f_end.add(controls, 'end_x', 0, 1000);
+    f_end.add(controls, 'end_y', 0, 1000);
+    f_end.add(controls, 'end_z', 0, 1000);
+    f_end.open();
     rotationSpeedController.onChange(
         function (value) {
             orbitControls.autoRotateSpeed = value;
