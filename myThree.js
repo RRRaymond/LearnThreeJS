0551b49941d31b0e06e4cb6a4251be2a46e72f5c
raymondchen <1552702@tongji.edu.cn>
2018-05-02 16:18:05 +0800

    add Bresenham algorithm and fix bugs

--- myThree.js
+++ myThree.js
@@ -65,18 +65,9 @@ function initLight() {
 
 var geometry;
 var material;
-var cube;
-var cube2;
 function initObject() {
-
     geometry = new THREE.CubeGeometry(0.95,0.95,0.95);
     material = new THREE.MeshLambertMaterial({color: 0x00ff00, wireframe: false});
-    cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0,1,0);
-    cube2 = new THREE.Mesh(geometry,material);
-    cube2.position.set(0,0,0);
-    scene.add(cube);
-    scene.add(cube2);
 }
 
 function render()
@@ -84,16 +75,19 @@ function render()
     delta = clock.getDelta();
     orbitControls.update(delta);
     requestAnimationFrame(render);
-    drawLine();
+    if(controls.Algorithm === "3DDDA")
+        drawLineWith3DDDA();
+    else{
+        drawLineWith3DBresenham();
+    }
     renderer.render(scene, camera);
 }
 
-function drawLine() {
+function drawLineWith3DDDA() {
     var children = scene.children;
     for(var i=3;i<children.length;i++){
         scene.remove(children[i]);
     }
-
     var directionVector = new THREE.Vector3(
         controls.end_x-controls.start_x,
         controls.end_y-controls.start_y,
@@ -117,11 +111,101 @@ function drawLine() {
         z += dz;
         scene.add(cube);
     }
+}
 
+function drawLineWith3DBresenham() {
+    var children = scene.children;
+    for(var i=3;i<children.length;i++){
+        scene.remove(children[i]);
+    }
+    var x1 = controls.start_x, y1 = controls.start_y, z1 = controls.start_z;
+    var x2 = controls.end_x, y2 = controls.end_y, z2 = controls.end_z;
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var dz = z2 - z1;
+    var x_inc = (dx < 0) ? -1 : 1;
+    var l = Math.abs(dx);
+    var y_inc = (dy < 0) ? -1 : 1;
+    var m = Math.abs(dy);
+    var z_inc = (dz < 0) ? -1 : 1;
+    var n = Math.abs(dz);
+    var dx2 = l * 2;
+    var dy2 = m * 2;
+    var dz2 = n * 2;
+
+    var x = x1, y = y1, z = z1;
+
+    if ((l >= m) && (l >= n)) {
+        var err_1 = dy2 - l;
+        var err_2 = dz2 - l;
+        for (var i = 0; i < l; i++) {
+            var cube = new THREE.Mesh(geometry, material);
+            cube.position.set(x, y, z);
+            scene.add(cube);
+            if (err_1 > 0) {
+                y += y_inc;
+                err_1 -= dx2;
+            }
+            if (err_2 > 0) {
+                z += z_inc;
+                err_2 -= dx2;
+            }
+            err_1 += dy2;
+            err_2 += dz2;
+            x += x_inc;
+            if(i === Math.round(l/2)){
+                orbitControls.target = new THREE.Vector3(x, y, z);
+            }
+        }
+    } else if ((m >= l) && (m >= n)) {
+        err_1 = dx2 - m;
+        err_2 = dz2 - m;
+        for (i = 0; i < m; i++) {
+            var cube = new THREE.Mesh(geometry, material);
+            cube.position.set(x, y, z);
+            scene.add(cube);
+            if (err_1 > 0) {
+                x += x_inc;
+                err_1 -= dy2;
+            }
+            if (err_2 > 0) {
+                z += z_inc;
+                err_2 -= dy2;
+            }
+            err_1 += dx2;
+            err_2 += dz2;
+            y += y_inc;
+            if(i === Math.round(m/2)){
+                orbitControls.target = new THREE.Vector3(x, y, z);
+            }
+        }
+    } else {
+        err_1 = dy2 - n;
+        err_2 = dx2 - n;
+        for (i = 0; i < n; i++) {
+            var cube = new THREE.Mesh(geometry, material);
+            cube.position.set(x, y, z);
+            scene.add(cube);
+            if (err_1 > 0) {
+                y += y_inc;
+                err_1 -= dz2;
+            }
+            if (err_2 > 0) {
+                x += x_inc;
+                err_2 -= dz2;
+            }
+            err_1 += dy2;
+            err_2 += dx2;
+            z += z_inc;
+            if(i === Math.round(n/2)){
+                orbitControls.target = new THREE.Vector3(x, y, z);
+            }
+        }
+    }
 }
 
 function threeStart() {
-    initGUI();
+
     initThree();
     initCamera();
     initScene();
@@ -131,7 +215,11 @@ function threeStart() {
 
 
     initOribitControls();
+
+    initGUI();
     render();
+
+
 }
 
 var orbitControls, clock, delta;
@@ -139,7 +227,7 @@ var orbitControls, clock, delta;
 function initOribitControls() {
     //添加轨道控制器
     //新建一个轨道控制器
-    orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+    orbitControls = new THREE.OrbitControls(camera, renderer.domElement, renderer.domElement);
     orbitControls.target = new THREE.Vector3(0, 0, 0);//控制焦点
     orbitControls.autoRotate = true;//将自动旋转打开
     orbitControls.autoRotateSpeed = 5;
@@ -161,6 +249,7 @@ function  initGUI() {
         this.end_x = 433;
         this.end_y = 985;
         this.end_z =666;
+        this.Algorithm = "3DDDA";
     };
 
     //创建dat.GUI，传递并设置属性
@@ -178,6 +267,7 @@ function  initGUI() {
     f_end.add(controls, 'end_y', 0, 1000);
     f_end.add(controls, 'end_z', 0, 1000);
     f_end.open();
+    gui.add(controls, "Algorithm", ["3DDDA", "3D Bresenham"]);
     rotationSpeedController.onChange(
         function (value) {
             orbitControls.autoRotateSpeed = value;
